54035fa3161b8451124355340c61216c0905d18b
evilz <evilznet@gmail.com>
2015-09-23 21:59:27 +0200

    first commit
    init

---
+++ brackets.js
@@ -0,0 +1,6 @@
+﻿var defaultBrackets = {
+    teams: [
+        [{ name: "Team 1", race: "T" }, { name: "Team 2", race: "Z" }],
+        [{ name: "Team 3", race: "P" }, { name: "Team 4", race: "Z" }]
+    ]
+};
