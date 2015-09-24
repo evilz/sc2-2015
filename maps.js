bbe8166eefa9c2387a0c2db9fdb8f748f3c3eb4e
evilz <evilznet@gmail.com>
2015-09-24 17:44:57 +0200

    Remove Map

---
+++ maps.js
@@ -0,0 +1,42 @@
+﻿var maps = [
+    {
+         name: 'Clair de lune fanatic',
+         img: 'clair-de-lune-fanatic.jpg',
+         maptype: '1v1'
+     },
+     {
+         name: 'Coda EC',
+         img: 'coda-ec.jpg',
+         maptype: '1v1'
+     },
+    {
+        name: 'Expedition perdue',
+        img: 'expedition-perdue.jpg',
+        maptype: '1v1'
+    },
+    {
+        name: 'Luxuriance EC',
+        img: 'luxuriance-ec.jpg',
+        maptype: '1v1'
+    },
+    {
+        name: 'Terminal stellaire',
+        img: 'terminal-stellaire.jpg',
+        maptype: '1v1'
+    },
+    {
+        name: 'Terraformation',
+        img: 'terraformation.jpg',
+        maptype: '1v1'
+    },
+    //{
+    //    name: 'Tête de pont',
+    //    img: 'tete-de-pont.jpg',
+    //    maptype: '2v2'
+    //},
+    {
+        name: 'Vallee des cactus ec',
+        img: 'vallee-des-cactus-ec.jpg',
+        maptype: '2v2'
+    }
+];
