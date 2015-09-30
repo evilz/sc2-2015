affb0e6501861f7d7681b07d846c93e174a3b30a
evilz <evilznet@gmail.com>
2015-09-30 22:18:13 +0200

    update score hellone/zimbali

--- brackets.js
+++ brackets.js
@@ -95,11 +95,18 @@
         race: "P",
         name: "HellOne"
     }, {
-        race: "",
+        race: "P",
         name: "Zimbali"
     }]],
     "results": [[[
-        [null,null],[null,null],[2,0],[0,null],[null,null],[null,null],[2,0],[0,null],[null,null],[null,null],[null,null],[null,null],[null,null],[null,null],[null,null],[null,null]],[[null,null],[null,null],[null,null],[null,null],[null,null],[null,null],[null,null],[null,null]],[[null,null],[null,null],[null,null],[null,null]],[[null,null],[null,null]],[[null,null],[null,null]
-    ]]
+        [null,null],[null,null],[2,0],[0,null],[null,null],[null,null],[2,0],[0,null],[null,null],[null,null],[null,null],[null,null],[null,null],[null,null],[null,null],[2,0]],[[null,null],[null,null],[null,null],[null,null],[null,null],[null,null],[null,null],[null,null]],[[null,null],[null,null],[null,null],[null,null]],[[null,null],[null,null]],[[null,null],[null,null]
+        ]]
     ]
 };
+
+{"teams":[[{"race":"","name":"warwan"},{"race":"","name":"F10p"}],[{"race":"","name":"motchi"},{"race":"","name":"Ulver"}],[{"race":"P","name":"crow"},{"race":"P","name":"MrGreen"}],[{"race":"","name":"durkan"},{"race":"","name":"Ombre"}],[{"race":"P","name":"KarlsBerg"},{"race":"","name":"ThgX"}],[{"race":"","name":"Samii"},{"race":"","name":"Rultane"}],[{"race":"P","name":"Evilz"},{"race":"T","name":"Air"}],[{"race":"Z","name":"Neths"},{"race":"","name":"skilo"}],[{"race":"","name":"fidzysss"},{"race":"","name":"ZGab25"}],[{"race":"T","name":"Vrowgh"},{"race":"","name":"tahitiBob"}],[{"race":"","name":"Mout"},{"race":"","name":"Limbshaft"}],[{"race":"","name":"Wayepi"},{"race":"T","name":"darkyokai"}],[{"race":"","name":"CoolSpot"},{"race":"","name":"safir"}],[{"race":"Z","name":"suistrop"},{"race":"","name":"Pulsions"}],[{"race":"","name":"Sebi"},{"race":"","name":"Bobo"}],[{"race":"P","name":"HellOne"},{"race":"","name":"Zimbali"}]],"results":[[[
+    
+    
+    [null,null],[null,null],[2,0],[0,null],[null,null],[null,null],[2,0],[0,null],[null,null],[null,null],[null,null],[null,null],[null,null],[null,null],[null,null],[2,0]],[[null,null],[null,null],[null,null],[null,null],[null,null],[null,null],[null,null],[null,null]],[[null,null],[null,null],[null,null],[null,null]],[[null,null],[null,null]],[[null,null],[null,null]
+
+]]]}
